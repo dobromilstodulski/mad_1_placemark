db9cd31f000ae5af49cd6de2677c497fbc46a425
Dobromil Stodulski <bb100dulski@gmail.com>
2021-11-23 21:39:21 +0000

    Image

--- app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -13,18 +13,24 @@ import timber.log.Timber
 import timber.log.Timber.i
 
 class PlacemarkActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityPlacemarkBinding
     var placemark = PlacemarkModel()
     lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         var edit = false
+
         binding = ActivityPlacemarkBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.toolbarAdd.title = title
         setSupportActionBar(binding.toolbarAdd)
+
         app = application as MainApp
+
+        i("Placemark Activity started...")
 
         if (intent.hasExtra("placemark_edit")) {
             edit = true
@@ -47,8 +53,13 @@ class PlacemarkActivity : AppCompatActivity() {
                     app.placemarks.create(placemark.copy())
                 }
             }
+            i("add Button Pressed: $placemark")
             setResult(RESULT_OK)
             finish()
+        }
+
+        binding.chooseImage.setOnClickListener {
+            i("Select image")
         }
     }
 
@@ -59,7 +70,9 @@ class PlacemarkActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.item_cancel -> { finish() }
+            R.id.item_cancel -> {
+                finish()
+            }
         }
         return super.onOptionsItemSelected(item)
     }
